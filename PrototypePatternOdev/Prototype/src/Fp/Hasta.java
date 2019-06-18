9cff45aa8aa8d070207ad0c11910cbae1edc6ebd
fehimeyigit <40387117+fehimeyigit@users.noreply.github.com>
2019-06-18 04:00:05 +0300

    Add files via upload

---
+++ PrototypePatternOdev/Prototype/src/Fp/Hasta.java
@@ -0,0 +1,40 @@
+package Fp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hasta implements Cloneable{
+
+	 private List<String> hastaListesi;
+	 
+	    public Hasta() {
+	    	hastaListesi = new ArrayList<String>();
+	    }
+	 
+	    public Hasta(List<String> liste) {
+	        this.hastaListesi = liste;
+	    }
+	 
+	    public void hastaEkle() {
+	 
+	    	hastaListesi.add("Burak");
+	    	hastaListesi.add("Ahmet");
+	    	hastaListesi.add("Mehmet");
+	    }
+	 
+	    public List<String> getHastaListesi() {
+	        return hastaListesi;
+	    }
+	 
+	    @Override
+	    public Object clone() throws CloneNotSupportedException {
+	        List<String> uyeListesi = new ArrayList<String>();
+	        for (String s : this.getHastaListesi()) {
+	            uyeListesi.add(s);
+	        }
+	        return new Hasta(uyeListesi);
+	    }
+	
+	
+	
+}
